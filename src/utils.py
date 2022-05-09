8f619eeb49ce5c25e11ba9398dbc01b9e733ad40
remigerme <remi.germe@gmail.com>
2022-05-09 15:27:05 +0200

    Clicking f a s t e r

--- src/utils.py
+++ src/utils.py
@@ -4,5 +4,5 @@ import win32api, win32con
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    time.sleep(0.001)
+    time.sleep(1e-5)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
